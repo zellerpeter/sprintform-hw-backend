ed2335168ec9f385d1681ef6fb3375439ec50588
Peter Zeller <zeli@quorra.net>
2023-11-03 20:59:58 +0100

    Project setup

---
+++ sprintform-hw-backend/src/main/java/com/sprintform/hw/SprintformHwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sprintform.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintformHwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintformHwBackendApplication.class, args);
+    }
+
+}
---
+++ sprintform-hw-backend/src/test/java/com/sprintform/hw/SprintformHwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintform.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintformHwBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
